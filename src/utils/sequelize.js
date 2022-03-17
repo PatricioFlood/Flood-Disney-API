8c7a661f2eb715628bc793f8e71dc67e7688915d
Patricio <pflood@frba.utn.edu.ar>
2022-03-17 17:00:08 -0300

    ssl

--- src/utils/sequelize.js
+++ src/utils/sequelize.js
@@ -1,13 +1,13 @@
 const { Sequelize } = require('sequelize');
 const config = require('./config')
 
+const dialectOptions = {}
+if(config.NODE_ENV === 'production') 
+  dialectOptions.ssl = { rejectUnauthorized: false }
+
 const sequelize = new Sequelize(config.DATABASE_URL, {
   logging: false,
-  dialectOptions: {
-    ssl: {
-      rejectUnauthorized: false
-    }
-  },
+  dialectOptions
 })
 
 sequelize.authenticate()
